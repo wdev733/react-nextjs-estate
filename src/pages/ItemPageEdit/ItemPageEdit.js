d653aafd08c5f4338406af858097b7fcc5110aa8
wdev733 <wdev733@gmail.com>
2017-03-27 06:33:30 +0600

    ItemPage Edit

---
+++ src/pages/ItemPageEdit/ItemPageEdit.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { inject } from 'mobx-react'
+import {
+  ItemPageInfo, ItemPageInfoEdit,
+  Container, ItemPageInfoScroller,
+  SliderImages
+} from 'components'
+import { ItemPageParametersContainer } from 'containers'
+import { randomNumber } from 'helpers'
+import s from './ItemPageEdit.sass'
+
+@inject(({items: {data}}) => ({
+  images: data ? data[0].images.gallery : null,
+  item: data[0]
+}))
+export default class ItemPageEdit extends Component {
+  state = {
+    shouldUpdate: 0
+  };
+
+  onChange = () => {
+    const shouldUpdate = randomNumber(1111,9999);
+
+    this.setState({
+      shouldUpdate
+    })
+  };
+
+  render() {
+    const {
+      props: {images, item},
+      state: {shouldUpdate},
+      onChange
+    } = this;
+    return (
+      <div>
+        <ItemPageParametersContainer onChange={onChange}
+                                     data={item.types} />
+
+        <ItemPageInfoScroller shouldUpdate={shouldUpdate} fixed={(
+          <SliderImages data={images}/>
+        )}>
+          {/*<ItemPageInfoEdit />*/}
+          <ItemPageInfoEdit className={s.info} />
+        </ItemPageInfoScroller>
+      </div>
+    )
+  }
+}
+
