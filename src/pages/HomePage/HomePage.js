f4f5e5e1fde34bc2cf1179986a01964a7cfe6e5c
wdev733 <wdev733@gmail.com>
2017-05-08 16:14:02 +0300

    Comment some unnecessary components at the point

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { inject, observer } from 'mobx-react'
 import {
-  Intro, BannerThreeCols, BannerTwoCols,
-  PricingBanner, PricingBannerFull,
+  Intro,
+  // BannerThreeCols, BannerTwoCols,
+  // PricingBanner, PricingBannerFull,
   ItemTilesBanner, MapItems, LoadingAnimation
 } from 'components'
 import s from './HomePage.sass'
@@ -13,20 +14,20 @@ const des =
   'Вы можете быть уверены, что данные квартиры соответствуют своему описанию, ' +
   'и документы, представленные реальными владельцами прошли верификацию.';
 
-const bannerThreeColsContent = [
-  {
-    title: 'Осмотр',
-    content: 'Все это выглядело бы чистым абсурдом, если бы не те ранние утра, когда, запив свой завтрак жидким чаем, я догонял трамвай.'
-  },
-  {
-    title: 'Проверка',
-    content: 'Чтобы добавить еще одну вишенку к темной людской грозди, свисавшей с подножки, и плыл сквозь акварельный розово-голубой город к конуре-проходной.'
-  },
-  {
-    title: 'Верификация',
-    content: 'Там два вахтера проверяли наши пропуска, а фасад был украшен классическими фанерными пилястрами.'
-  }
-];
+// const bannerThreeColsContent = [
+//   {
+//     title: 'Осмотр',
+//     content: 'Все это выглядело бы чистым абсурдом, если бы не те ранние утра, когда, запив свой завтрак жидким чаем, я догонял трамвай.'
+//   },
+//   {
+//     title: 'Проверка',
+//     content: 'Чтобы добавить еще одну вишенку к темной людской грозди, свисавшей с подножки, и плыл сквозь акварельный розово-голубой город к конуре-проходной.'
+//   },
+//   {
+//     title: 'Верификация',
+//     content: 'Там два вахтера проверяли наши пропуска, а фасад был украшен классическими фанерными пилястрами.'
+//   }
+// ];
 const mapStateToProps = ({items: {isFetching}, user}) => ({
   isFetching: isFetching || user.isFetching
 });
@@ -46,11 +47,11 @@ export default class HomePage extends Component {
         <Intro />
         <MapItems />
         <ItemTilesBanner />
-        <PricingBanner />
-        <PricingBannerFull />
-        <BannerThreeCols content={bannerThreeColsContent}
-                         title="Услуги"/>
-        <BannerTwoCols />
+        {/*<PricingBanner />*/}
+        {/*<PricingBannerFull />*/}
+        {/*<BannerThreeCols content={bannerThreeColsContent}
+                         title="Услуги"/>*/}
+        {/*<BannerTwoCols />*/}
         {isFetching && <LoadingAnimation />}
       </div>
     )
